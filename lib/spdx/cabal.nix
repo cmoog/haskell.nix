777dbff998cceecc0726f9a2a213b60bbaf824e5
Charlie Moog <cmoog@anduril.com>
2024-02-27 16:19:48 -0600

    rm license trace

--- lib/spdx/cabal.nix
+++ lib/spdx/cabal.nix
@@ -7,9 +7,7 @@ let
   # let block below (probably helps by increasing
   # the sharing)
   license-map = import ../cabal-licenses.nix pkgs;
-  otherLicenseWarning = lic:
-    trace "WARNING: license \"${lic}\" not found"
-          license-map.LicenseRef-OtherLicense;
+  otherLicenseWarning = lic: license-map.LicenseRef-OtherLicense;
 in license:
 let
   licenses = spdx.compound-expression license;
